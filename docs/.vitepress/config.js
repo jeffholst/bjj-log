19856480a243d81c24d816a320d3b12e1754b93e
Jeff Holst <33558430+jeffholst@users.noreply.github.com>
2023-01-16 19:03:26 -0600

    chore: added config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,25 @@
+export default {
+  title: `BJJ Log`,
+  description: `Jeff's BJJ Log`,
+  head: [],
+  lastUpdated: true,
+  themeConfig: {
+    editLink: {
+      pattern: "https://github.com/jeffholst/bjj-log/edit/main/docs/:path",
+      text: "Edit this page on GitHub",
+    },
+    nav: [],
+    sidebar: [
+      {
+        text: "About",
+        collapsible: true,
+        items: [],
+      },
+      {
+        text: "2023",
+        collapsible: true,
+        items: [],
+      },
+    ],
+  },
+}
